7313563437b90c88e523214a42e68d2eb99ea545
AmirHossein Habibvand <ahhabibvand@gmail.com>
2019-05-13 19:33:33 +0430

    Add testbench

---
+++ Tb.sv
@@ -0,0 +1,15 @@
+`timescale 1ns/1ns
+
+module TB();
+	reg rst, clk = 0;
+	CPU processor (clk, rst);
+	always #10 clk = ~clk;
+	initial begin
+		#100;
+		rst = 1;
+		#100;
+		rst = 0;
+		#10000;
+		$stop;
+	end
+endmodule
