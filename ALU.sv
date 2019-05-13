b0dea97f17f06f7cf2dcecc89c23c711b22bbf5a
AmirHossein Habibvand <ahhabibvand@gmail.com>
2019-05-13 14:32:22 +0430

    Add an ALU

---
+++ ALU.sv
@@ -0,0 +1,12 @@
+module ALU( input        [7:0] data_i , data_j ,
+            input        [1:0] aluOp ,
+            output  reg  [7:0] result ) ;
+            always @(*) begin
+              case( aluOp ) 
+                7'b00 : result = data_i + data_j ;
+                7'b01 : result = data_i - data_j ;
+                7'b10 : result = data_i & data_j ;
+                7'b11 : result = ~data_i ;
+              endcase
+            end
+endmodule
