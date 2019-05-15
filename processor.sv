046818989a108be3cd6ffa68df0234dd584c89d1
AmirHossein Habibvand <ahhabibvand@gmail.com>
2019-05-15 09:42:22 +0430

    Add rst to datapath

--- processor.sv
+++ processor.sv
@@ -1,10 +1,10 @@
 `timescale 1ns/1ns
 
 module Processor(input clk, rst);
-  wire ld_IR, PCorIR, push, pop, MEMorALU, ldA, ldB, PCup, PCwrite, J, JZ, write_enable ;
+  wire ld_IR, PCorIR, push, pop, MEMorALU, ldA, ldB, PCup, PCwrite, J, JZ, write_enable;
   wire [1:0] ALUop ;
   wire [2:0] inst ;
 
   Controller ct( clk, rst, inst, ld_IR, PCorIR, push, pop, MEMorALU, ldA, ldB, PCup, PCwrite, J, JZ, write_enable, ALUop);
-  Datapath dp( clk, ld_IR, PCorIR, push, pop, MEMorALU, ldA, ldB, PCup, PCwrite, J, JZ, write_enable, ALUop, inst);
+  Datapath dp( clk, rst, ld_IR, PCorIR, push, pop, MEMorALU, ldA, ldB, PCup, PCwrite, J, JZ, write_enable, ALUop, inst);
 endmodule
